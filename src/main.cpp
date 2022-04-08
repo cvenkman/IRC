2b0b67a99a93d205dbe67ebcf359a7fd353fd9a6
Carlos Venkman <cvenkman@si-f3.kzn.21-school.ru>
2022-04-09 02:50:49 +0300

    done with select

--- src/main.cpp
+++ src/main.cpp
@@ -104,6 +104,14 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    fd_set activeSet, readSet; // множества дескрипторов для select
+    FD_ZERO(&activeSet);
+    /*
+     * в activeSet устанавливаем 1 по тому номеру, который соотвествует socketFd
+     * т.е. сейчас в activeSet один сокет socketFd, который принимает запросы на соединение
+    */
+    FD_SET(socketFd, &activeSet); // FIXME
+
     /********************************
             про accept()
     int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen); 
@@ -117,51 +125,112 @@ int main(int argc, char **argv)
        а вновь созданный полностью готов к send() и recv()
     ********************************/
 
-// где-то здесь нужно начать бесконечный цикл
+    struct sockaddr_in clientAddr; // для accept()
+    // socklen_t clientAddrLen = sizeof(clientAddr);
+    int newFd;
 
-    struct sockaddr_in clientaddr;
-    socklen_t len = sizeof(clientaddr);
+    while (1) {
+        /* FD_SETSIZE - максимальное кол-во каналов в системе,
+         * т.е. рассматриваем столько элементов, сколько множество содержит
+        */
 
-    /*
-     * теперь у нас есть socketFd и newFd:
-     * на socketFd слушать
-     * на newFd новое подключения
-    */
-    int newFd = accept(socketFd, (struct sockaddr *)&clientaddr, &len); // украла эту строку из экзамена
-    if (newFd == -1) {
-        perror("accept");
-        return 1;
+        /********************************
+            про select()
+        int select(int n, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
+        параметры по порядку:
+        * сколько элементов просматривать в множестве
+        * множество которое нужно проверять на возможное чтение
+        * множество которое нужно проверять на возможную запись
+        * множество которое нужно проверять на наличие исключительных ситуаций
+        * таймаут в течении которого надо с этим работать (если NULL, то висим на этой функции
+            пока один из каналов в множестве activeSet не проявит активность)
+
+        * блокируемся на select, а не на accept
+        ********************************/
+
+        readSet = activeSet; // делаем копию т.к. select() изменяет множество, а нам дальеш нужно знать, кого читать
+        if (select(FD_SETSIZE, &readSet, NULL, NULL, NULL) < 0) { //FIXME
+            perror("select");
+            return 1;
+        }
+        // на этой строчке у нас либо возникла ошибка в select, либо появились данные в одном из каналов readSet
+
+        // нужно проверить в каком сокете (в каком элементе readSet) появились данные (т.е. 1)
+        for (int i = 0; i < FD_SETSIZE; i++) {
+            if (FD_ISSET(i, &readSet)) {
+                if (i == socketFd) {
+                    // подтверждение соединения
+                    // пришел запрос на новое соединение (клиент выполнил connect => мы должны сделать accept)
+                    socklen_t clientAddrLen = sizeof(clientAddr);
+                    newFd = accept(socketFd, (struct sockaddr *)&clientAddr, &clientAddrLen); // украла эту строку из экзамена
+                    if (newFd == -1) {
+                        perror("accept");
+                        return 1;
+                    }
+                    std::cout << "Server: new connect; client's fd: " << newFd << "\n";
+                    /* новый сокет newFd добавили в множество активных сокетов activeSet (установить в 1 newFd в activeSet) */
+                    FD_SET(newFd, &activeSet);
+                }
+                else {
+                    // пришли данные в уже существующем соединение и нам нужно работать с клиентом
+                    
+                    /* читаем сообщение от клиента */
+                    char buf[100];
+                    bzero(&buf, 100);
+                    int readBytes = recv(i, &buf, 100, 0);
+                    /*
+                    * recv() может возвращать 0.
+                    * Это означает, что удалённая сторона (клиент) закрыла для вас подключение
+                    */
+                    if (readBytes == -1) {
+                        close(i);
+                        FD_CLR(i, &activeSet); // удаляем из активного множества
+                        perror("recv");
+                    }
+                    else if (!strncmp(buf, "stop", 4)) {
+                        close(i);
+                        FD_CLR(i, &activeSet);
+                    }
+                    else {
+                        /* отправляем сообщение клиенту */
+                        std::string msg = "hi i'm a message for a client with fd=" + std::to_string(i);
+                        msg += ", your message: ";
+                        msg += buf;
+                        int sentBytes = send(i, msg.c_str(), msg.length(), 0);
+                        /* sentBytes может быть меньше msg.length(), хз пока что с этим делать */
+                        if (sentBytes == -1) {
+                            perror("send");
+                            return 1;
+                        }
+                    }
+                    std::cout << "From client (fd = " << i << "): " << buf << "\n";
+                }
+            }
+        }
+
+        // /*
+        // * теперь у нас есть socketFd и newFd:
+        // * на socketFd - слушать запросы на соединение
+        // * на newFd - обслуживать соединение (новое подключение) конкретного клиента
+        // */
+        // int newFd = accept(socketFd, (struct sockaddr *)&clientaddr, &len); // украла эту строку из экзамена
+        // if (newFd == -1) {
+        //     perror("accept");
+        //     return 1;
+        // }
+
+
+
+
+
+        // close(newFd); // shutdown() (но после него все равно нужно вызвать close)
     }
-
-    /* отправляем сообщение клиенту */
-    std::string msg = "hi i'm a message for a client\n";
-    int sentBytes = send(newFd, msg.c_str(), msg.length(), 0);
-    /* sentBytes может быть меньше msg.length(), хз пока что с этим делать */
-    if (sentBytes == -1) {
-        perror("send");
-        return 1;
-    }
-
-    /* читаем сообщение от клиента */
-    char buf[100];
-    bzero(&buf, 100);
-    int readBytes = recv(newFd, &buf, 100, 0);
-    /*
-     * recv() может возвращать 0.
-     * Это означает, что удалённая сторона (клиент) закрыла для вас подключение
-    */
-    if (readBytes == -1) {
-        perror("recv");
-        return 1;
-    }
-    std::cout << buf << "\n";
 
 
 //getpeername - это не пробовала
 //gethostname - это не работает
 
 
-    close(newFd); // shutdown() (но после него все равно нужно вызвать close)
     close(socketFd);
     return 0;
 }
